ad6ac83cd59afc6e3330e3b95f471b0d9ea26772
ppanzenboeck <philipp.panzenboeck1@gmail.com>
2023-12-01 15:26:38 +0100

    removed even more packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,27 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := YouTube Maps SafetyHubPrebuilt Drive PrebuiltGmail FilesPrebuilt CalculatorGooglePrebuilt CalendarGooglePrebuilt Chrome-Stub GoogleContacts GoogleTTS Photos WellbeingPrebuilt
+LOCAL_OVERRIDES_PACKAGES += \
+	YouTube \
+	Maps \
+	SafetyHubPrebuilt \
+	Drive \
+	PrebuiltGmail \
+	FilesPrebuilt \
+	CalculatorGooglePrebuilt \
+	CalendarGooglePrebuilt \
+	Chrome-Stub \
+	GoogleContacts \
+	GoogleTTS \
+	Photos \
+	WellbeingPrebuilt \
+	RecorderPrebuilt \
+	PixelWallpapers2020 \
+	PixelLiveWallpaperPrebuilt \
+	PixelThemesStub \
+	DocumentsUIGoogle \
+	GoogleTTS
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null

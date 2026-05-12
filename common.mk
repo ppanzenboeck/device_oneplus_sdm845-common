2dd702a3457beaf0b6e207c529fa1f3c3ca9d6b6
ppanzenboeck <panz.development@gmail.com>
2026-05-12 21:42:31 +0200

    fixup! fix media preview on third party apps such as Telegram

--- common.mk
+++ common.mk
@@ -28,7 +28,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Media Thumbnails loading fix
-$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+$(call soong_config_set_bool,stagefright,target_disable_thumbnail_block_model,true)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
